cd329a44a208c97234b8fa49f6cbbb1568cba285
Can Ibanoglu <can.ibanoglu@gmail.com>
2019-11-05 00:22:14 +0100

    Revert "handle single file torrents"
    This reverts commit c27b91a3

--- bin/createTorrent.js
+++ bin/createTorrent.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 import { checkEnvironmentVariables } from '../utils';
 
+checkEnvironmentVariables();
+
 const path = require('path');
 const createTorrent = require('create-torrent');
 const parseTorrent = require('parse-torrent');
 const fs = require('fs').promises;
-const fsStatSync = require('fs').statSync;
 const argv = require('minimist')(process.argv.slice(2));
 
 const {
   addTorrent: addTorrentToDatabase
 } = require('../databaseOperations.js');
 
-checkEnvironmentVariables();
+// TODO: Check for env variables and fail if not present with instructions
 
 const {
   CAN_TRACKER_ANNOUNCE_URL,
@@ -41,26 +42,14 @@ async function writeTorrentFile(fileName, torrentBuffer) {
       });
 }
 
-let webSeedUrl = CAN_TRACKER_WEB_SEED_URL;
 
-if (argv._.length === 1) {
-    const [ fileName ] = argv._;
-    const stat = fsStatSync(fileName);
-    if (stat.isFile()) {
-        const split = fileName.split(path.sep);
-        const name = split[split.length - 1];
-        webSeedUrl = path.join(CAN_TRACKER_WEB_SEED_URL, name);
-    }
-}
-
-console.log(argv._);
 console.info("Creating torrent...");
 createTorrent(argv._, {
   private: true,
   announceList: [
       [CAN_TRACKER_ANNOUNCE_URL],
   ],
-  urlList: [webSeedUrl],
+  urlList: [CAN_TRACKER_WEB_SEED_URL],
 }, (err, torrentBuffer) => {
   if (err) throw err;
 
